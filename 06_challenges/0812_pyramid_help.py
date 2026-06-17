21a1df49b51060cd60f154ae1bbeae0e33a20581
fifi <70396@edu.campusvejle.dk>
2026-06-17 14:33:13 +0200

    new

---
+++ 06_challenges/0812_pyramid_help.py
@@ -0,0 +1,50 @@
+"""Opgave "Number pyramid"
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+--------
+
+Denne øvelse er en valgfri udfordring for de fremragende programmører blandt jer.
+Du behøver absolut ikke at løse denne øvelse for at fortsætte med succes.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Del 1:
+    Se de første 93 sekunder af denne video: https://www.youtube.com/watch?v=NsjsLwYRW8o
+
+Del 2:
+    Skriv en funktion "pyramid", der producerer de tal, der er vist i videoen.
+    Funktionen har en parameter "lines", der definerer, hvor mange talrækker der skal produceres.
+    Funktionen udskriver tallene i hver række og også deres sum.
+
+Del 3:
+    I hovedprogrammet kalder du funktionen med fx 7 som argument.
+
+Del 4:
+    Tilføj en mere generel funktion pyramid2.
+    Denne funktion har som andet parameter "firstline" en liste med pyramidens øverste rækkens tallene.
+
+Del 5:
+    I hovedprogrammet kalder du pyramid2 med fx 10 som det første argument
+    og en liste med tal efter eget valg som andet argument.
+    Afprøv forskellige lister som andet argument.
+
+--------
+
+Hvis du går i stå, så spørg google, de andre elever, en AI eller læreren.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+"""
+
+
+def pyramid(lines):
+    numbers = [1, 1]
+    0
+
+
+def pyramid2(lines, firstline):
+    0
+
+
+pyramid(7)
+pyramid2(7, [1, 0, 1, 2])
---
+++ 06_challenges/0820_inventory_exercise.py
@@ -0,0 +1,34 @@
+"""Opgave "The inventory sequence"
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+--------
+
+Denne øvelse er en valgfri udfordring for de fremragende programmører blandt jer.
+Du behøver absolut ikke at løse denne øvelse for at fortsætte med succes.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Del 1:
+    Se de første 3 minutter af denne video:
+    https://www.youtube.com/watch?v=rBU9E-ZOZAI
+
+Del 2:
+    Skriv en funktion inventory(), som producerer de tal, der er vist i videoen.
+    Funktionen accepterer en parameter, der definerer, hvor mange talrækker der skal produceres.
+    Funktionen udskriver tallene i hver række.
+
+    Du vil sandsynligvis ønske at definere en funktion count_number(), som tæller, hvor ofte
+    et bestemt antal optræder i den aktuelle talrække.
+
+Del 3:
+    I hovedprogrammet kalder du inventory() med fx 6 som argument.
+
+--------
+
+Hvis du går i stå, så spørg google, de andre elever, en AI eller læreren.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+"""
+
+
