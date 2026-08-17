3b6601880ddd229a2577c38686c80acb630c13f7
fifi <70396@edu.campusvejle.dk>
2026-08-17 11:14:18 +0200

    made user inputs int

--- 05_practice/0700_calculator_exercise.py
+++ 05_practice/0700_calculator_exercise.py
@@ -26,6 +26,7 @@ Hvis du går i stå, spørg Google, andre elever, en AI eller læreren.
 Når dit program er færdigt, skub det til dit GitHub-repository.
 """
 
+
 ongoing = True
 while ongoing:
     print(f"This is a calculator.\n\n- Type '1' to use addition\n- Type '2' to use subtraction\n- Type '3' to use multiplication\n"
@@ -33,24 +34,24 @@ while ongoing:
     action = input("Pick an action and press Enter.\n")
     if action == "1":
         print("\nYou picked addition.\n")
-        number1 = input("Type in the first number of the equation. Example: (First number) + (Second number).\n")
-        number2 = input("Type in the second number of the equation.\n")
-        print(f"\n{int(number1)} + {int(number2)} = {int(number1)+int(number2)}\n")
+        number1 = int(input("Type in the first number of the equation. Example: (First number) + (Second number).\n"))
+        number2 = int(input("Type in the second number of the equation.\n"))
+        print(f"\n{number1} + {number2} = {number1+number2}\n")
     elif action == "2":
         print("\nYou picked subtraction.\n")
-        number1 = input("Type in the first number of the equation. Example: (First number) - (Second number).\n")
-        number2 = input("Type in the second number of the equation.\n")
-        print(f"\n{int(number1)} - {int(number2)} = {int(number1)-int(number2)}\n")
+        number1 = int(input("Type in the first number of the equation. Example: (First number) - (Second number).\n"))
+        number2 = int(input("Type in the second number of the equation.\n"))
+        print(f"\n{number1} - {number2} = {number1-number2}\n")
     elif action == "3":
         print("\nYou picked multiplication.\n")
-        number1 = input("Type in the first number of the equation. Example: (First number) * (Second number).\n")
-        number2 = input("Type in the second number of the equation.\n")
-        print(f"\n{int(number1)} * {int(number2)} = {int(number1)*int(number2)}\n")
+        number1 = int(input("Type in the first number of the equation. Example: (First number) * (Second number).\n"))
+        number2 = int(input("Type in the second number of the equation.\n"))
+        print(f"\n{number1} * {number2} = {number1*number2}\n")
     elif action == "4":
         print("\nYou picked division.\n")
-        number1 = input("Type in the first number of the equation. Example (First number) / (Second number).\n")
-        number2 = input("Type in the second number of the equation.\n")
-        print(f"\n{int(number1)} / {int(number2)} = {int(number1)/int(number2)}\n")
+        number1 = int(input("Type in the first number of the equation. Example (First number) / (Second number).\n"))
+        number2 = int(input("Type in the second number of the equation.\n"))
+        print(f"\n{number1} / {number2} = {number1/number2}\n")
     elif action == "0":
         print("\nYou ended the program.\n")
         ongoing = False
